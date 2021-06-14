3410aa450cb6575682081c56beb9a2271331a384
Israel Sanchez <50373290+IsrraelS@users.noreply.github.com>
2021-06-14 19:08:03 +0200

    server.js: express /  db.js: moongose

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+const path = require("path");
+
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+const connectDB = require("./config/db");
+
+const app = express()
+
+// Connect Database
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+ // Define Routes
+
+ // Middleware
+app.use(notFound);
+
+ const PORT = process.env.PORT || 5000;
+
+ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
